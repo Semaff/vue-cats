4529600f7b341d5f348c7d6e5de0adcd8f2f6bfb
Semyon <semarc-zhu@yandex.ru>
2024-09-08 20:54:12 +0300

    Add ui components

---
+++ src/ui/MyInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="my-input">
+    <label v-if="label" :for="id">{{ label }}</label>
+    <input v-model="model" :id="id" :type="type" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+  },
+  type: {
+    type: String,
+    default: 'text',
+  },
+});
+
+const model = defineModel();
+</script>
+
+<style scoped lang="scss">
+.my-input {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  label {
+    font-size: 18px;
+    color: #000;
+  }
+
+  input {
+    padding: 12px;
+    border: 1px solid #000;
+    border-radius: 4px;
+  }
+}
+</style>
---
+++ src/ui/MySelect.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="my-select">
+    <label v-if="label" :for="id">{{ label }}</label>
+    <select v-model="model" :name="id" :id="id">
+      <option v-for="option in options" :key="option.value" :value="option.value">
+        {{ option.label }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script setup lang="ts">
+export interface SelectOption<T = string> {
+  value: T;
+  label: string;
+}
+
+interface SelectProps {
+  id: string;
+  label?: string;
+  options: SelectOption[];
+}
+
+defineProps<SelectProps>();
+
+const model = defineModel();
+</script>
+
+<style scoped lang="scss">
+.my-select {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+
+  label {
+    font-size: 18px;
+    color: #000;
+  }
+
+  select {
+    padding: 12px;
+    border: 1px solid #000;
+    border-radius: 4px;
+  }
+}
+</style>
