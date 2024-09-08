c6d200e39acec5b3ab6fcc6fb47ffc2d7dea95da
Semyon <semarc-zhu@yandex.ru>
2024-09-08 20:54:22 +0300

    Add MyHeader component

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -1,7 +1,27 @@
 <template>
-  <div></div>
+  <header class="header">
+    <div class="header__inner">
+      <h1>Котикиии</h1>
+      <span>Профиль</span>
+    </div>
+  </header>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped></style>
+<style scoped lang="scss">
+.header {
+  border-bottom: 1px solid #e1e1e1;
+}
+
+.header__inner {
+  width: 100%;
+  max-width: 1280px;
+  padding: 40px;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
